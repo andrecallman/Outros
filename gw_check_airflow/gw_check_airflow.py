135eec6a058914fdc452cee3161a08528fe6da96
andrecallman <andreluisantunes@gmail.com>
2022-07-22 09:26:31 -0300

    commit

--- gw_check_airflow/gw_check_airflow.py
+++ gw_check_airflow/gw_check_airflow.py
@@ -2,12 +2,11 @@ import requests
 
 
 url = 'http://192.168.50.91:8080/api/v1/dags/example_bash_operator/tasks'
-url = 'http://airflow.integration.cloud.bionexo.com.br/api/experimental/dags/plannexo_integration_company_104/dag_runs'
 username = 'admin'
 password = 'a123456@'
 
 username='andre.antunes@bionexo.com'
-password = 'cwbjj214m*'
+password = '*******'
 
 headers = {
     'Content-Type': 'application/json',
